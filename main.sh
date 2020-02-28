b5ec142031bf973a80d42d82d23cc20663ea1010
Gabe Cook <gabe@clevyr.com>
2020-02-28 14:44:40 -0600

    :sparkles: Add new envs for Google Cloud login before updating deployment image

--- main.sh
+++ main.sh
@@ -1,11 +1,17 @@
+#!/usr/bin/env bash
+
 set -euxo pipefail
 
-# Create a file with the kubeconfig
-echo $KUBECONFIGB64 | base64 -d > .kubeconfig
-export KUBECONFIG=.kubeconfig
+# Create a file with the Google Cloud Auth Key File
+gcloud auth activate-service-account \
+    --key-file - <<< $GCLOUD_KEY_FILE
+
+gcloud container clusters get-credentials \
+    $GCLOUD_CLUSTER_NAME \
+    --region $GCLOUD_REGION
 
 # Push update to application through helm
-kubectl set image deployments/$CHART_TITLE $CHART_TITLE=$REPO_URL:$REPO_TAG
+kubectl set image \
+    deployments/$CHART_TITLE \
+    $CHART_TITLE=$REPO_URL:$REPO_TAG
 
-# Delete the kubeconfig file
-rm .kubeconfig
