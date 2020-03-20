1984f6836d006136441f8d7f02a6afec42052902
Gabe Cook <gabe@clevyr.com>
2020-03-20 15:45:57 -0500

    :sparkles: Add rollout command so that CI watches deployments and waits for the rollout to be complete

--- main.sh
+++ main.sh
@@ -4,18 +4,20 @@ set -euxo pipefail
 
 # Create a file with the Google Cloud Auth Key File
 gcloud auth activate-service-account \
-    --key-file - <<< $GCLOUD_KEY_FILE
+    --key-file - <<< "$GCLOUD_KEY_FILE"
 
-project_id="$(jq -r .project_id <<< $GCLOUD_KEY_FILE)"
+project_id="$(jq -r .project_id <<< "$GCLOUD_KEY_FILE")"
 project_id=${GCLOUD_GKE_PROJECT:-$project_id}
 
 gcloud container clusters get-credentials \
-    $GCLOUD_CLUSTER_NAME \
-    --region $GCLOUD_REGION \
-    --project $project_id
+    "$GCLOUD_CLUSTER_NAME" \
+    --region "$GCLOUD_REGION" \
+    --project "$project_id"
 
 # Push update to application through kubectl
-kubectl set image \
-    deployments/$KUBE_NAMESPACE \
-    "*=$REPO_URL:$REPO_TAG" \
-    --namespace $KUBE_NAMESPACE
+kubectl -n "$KUBE_NAMESPACE" set image "deployments/$KUBE_NAMESPACE" "*=$REPO_URL:$REPO_TAG"
+
+if ! kubectl -n "$KUBE_NAMESPACE" rollout status deployment "$KUBE_NAMESPACE"; then
+    kubectl -n "$KUBE_NAMESPACE" rollout undo deployment "$KUBE_NAMESPACE"
+    kubectl -n "$KUBE_NAMESPACE" rollout status deployment "$KUBE_NAMESPACE"
+fi
