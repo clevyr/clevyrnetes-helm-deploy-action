4d9b8ef0d3d58c53bd0d7605a9c68bef6eb17725
Keith Hand <keith@clevyr.com>
2020-08-06 11:16:58 -0500

    :sparkles: Remove necessity for gcr-parse-action

--- main.sh
+++ main.sh
@@ -24,11 +24,31 @@ gcloud auth activate-service-account \
 
 # Set helm url based on default, or use provided HELM_URL variable
 default_helm_url="https://helm.clevyr.cloud"
-HELM_URL="${HELM_URL:-$default_helm_url}"
+helm_url="${HELM_URL:-$default_helm_url}"
+
+# Set the host project based on the value provided or based on the default
+default_host_project="momma-motus"
+host_project="${HOST_PROJECT:-$default_host_project}"
 
 # Set the project id based on the key file provided, or use the provided project id
 project_id="$(jq -r .project_id <<< "$GCLOUD_KEY_FILE")"
 project_id="${GCLOUD_GKE_PROJECT:-$project_id}"
+
+# Set the cluster name based on the key file provided unless it is provided
+cluster_name=$(gcloud container --project "$host_project" clusters list --format json \
+                | jq '.[]["name"]' \
+                | xargs)
+cluster_name="${GCLOUD_CLUSTER_NAME:-$cluster_name}"
+
+# Set the region tag unless it is provided
+region=$(gcloud container --project "$host_project" clusters list --format json \
+          | jq '.[]["zone"]' \
+          | xargs)
+region="${GCLOUD_REGION:-$region}"
+
+# Set the default us gcr docker repo unless another is provided
+docker_repo="us.gcr.io/$project_id"
+docker_repo="${REPO_URL:-$docker_repo}"
 
 # Set the environment label based on the last id in the KUBE_NAMESPACE, or use the provided ENV_LABEL variable
 environment="${KUBE_NAMESPACE##*-}"
@@ -41,7 +61,7 @@ config_folder="${CONFIG_FOLDER:-$config_folder}"
 # Select kubernetes cluster specified by GCLOUD_CLUSTER_NAME
 gcloud container clusters get-credentials \
     "$GCLOUD_CLUSTER_NAME" \
-    --region "$GCLOUD_REGION" \
+    --region "$region" \
     --project "$project_id"
 
 # Set the kubectl context namespace
@@ -51,7 +71,7 @@ kubectl config set-context --current --namespace="$KUBE_NAMESPACE"
 deployment="$KUBE_NAMESPACE${DEPLOYMENT_MODIFIER:+-$DEPLOYMENT_MODIFIER}"
 
 # Add custom helm repo
-helm repo add clevyr "$HELM_URL"
+helm repo add clevyr "$helm_url"
 helm repo update
 
 # Wait to make sure yq is installed
@@ -61,7 +81,7 @@ wait
 start_update main
 helm upgrade "$deployment" clevyr/"$(yq r "$config_folder"/"$environment"/helm.yaml 'app.framework')"-chart \
     -f "$config_folder"/"$environment"/helm.yaml \
-    --set app.image.url="$REPO_URL" \
+    --set app.image.url="$docker_repo" \
     --set app.image.tag="$REPO_TAG" \
     --wait
 end_update
